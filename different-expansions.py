44b8d7a529ed7bd2f96bc6657ecded291671bb9c
NY77NW <ny77nw@cs.aau.dk@a512-ib1-a-01.srv.aau.dk>
2025-04-24 15:58:49 +0200

    scripts for cluster

--- different-expansions.py
+++ different-expansions.py
@@ -3,8 +3,9 @@ from contrastive_regression import *
 from environments import *
 from properties import *
 
-run_env = LocalRunEnvironment(Path("~/Documents/Phd/conan/conan-dev/builds/release/bin/downward").expanduser())
-# run_env = MCCCplexRunEnvironment(Path("~/singularity_images/conan-dev-115d23909.img").expanduser())
+#run_env = LocalRunEnvironment(Path("~/Documents/Phd/conan/conan-dev/builds/release/bin/downward").expanduser())
+run_env = MCCCplexRunEnvironment(Path("/nfs/home/cs.aau.dk/ny77nw/singularity_images/conan-dev-8802afe1b.img").expanduser())
+
 
 BUGGY_CONFIGURATION = "propagation(eval=propagation_evaluator(eval=blind()))"
 GT_CONFIGURATION = "astar(blind(), pruning=dominance(database=previous_lower_g()))"
